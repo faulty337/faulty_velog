d5c475dce98e3832148ea29949e8910a281ad824
DESKTOP-A6GU9RJ\fault <faulty337@gmail.com>
2024-09-28 22:05:56 +0900

    Refactor : 환경 변수 이용

--- src/main/java/com/faulty/Main.java
+++ src/main/java/com/faulty/Main.java
@@ -4,17 +4,19 @@ import java.util.Map;
 
 public class Main {
 
-    private static String repoPath = ".";
-    private static String postsDir = "velog-posts";
-    private static String velogUrl = "https://api.velog.io/rss/@faulty337";
     public static void main(String[] args) {
+        String repoPath = System.getProperty("REPO_PATH");
+        String postsDir = System.getProperty("POSTS_DIR");
+        String velogUrl = System.getProperty("RSS_URL");
+        
         VelogRSSFetcher velogRSSFetcher = new VelogRSSFetcher(velogUrl);
         Map<String, String> feeds = velogRSSFetcher.getFeedSource();
 
         FileSaver fileSaver = new FileSaver();
         for(String key : feeds.keySet()) {
             try{
-                fileSaver.saveMarkdown(repoPath, postsDir, key.replace(".md", ""), feeds.get(key));
+                String fileName = key.endsWith(".md") ? key.substring(0, key.length() - 3) : key; //key에 .md 확장자 붙어 있음
+                fileSaver.saveMarkdown(repoPath, postsDir, fileName, feeds.get(key));
             }catch(Exception e){
                 System.out.println(e.getMessage());
             }
